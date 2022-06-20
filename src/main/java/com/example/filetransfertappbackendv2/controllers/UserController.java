add083e1ed99a2329698ead0c37019e9990cdbda
“RABHI-Amine” <mohammedaminerabhi17@gmail.com>
2022-06-20 09:28:35 +0100

    implementing user creation method

--- src/main/java/com/example/filetransfertappbackendv2/controllers/UserController.java
+++ src/main/java/com/example/filetransfertappbackendv2/controllers/UserController.java
@@ -4,27 +4,31 @@ import com.example.filetransfertappbackendv2.entities.Transfert;
 import com.example.filetransfertappbackendv2.entities.User;
 import com.example.filetransfertappbackendv2.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController()
 @RequiredArgsConstructor
 @CrossOrigin("*")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping
     List<User> getTransferts(){
         return userService.getAllUsers();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     User getUser(@PathVariable Long id){
         return userService.findUserById(id);
     }
+
+    @PostMapping
+    User createUser(@RequestBody User user){
+        return userService.save(user);
+    }
+
 }
